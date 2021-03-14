3644ec46aff12b83b41541745c375495177e58c0
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-14 15:04:58 -0400

    Manejo de errores promesas

--- 01-fundamentos/promise-2.js
+++ 01-fundamentos/promise-2.js
@@ -1,7 +1,13 @@
 function sumarUno( numero ) {
 
     var promesa = new Promise((resolve, reject) => {
- 
+
+        console.log( numero );
+
+        if( numero >= 7 ) {
+            reject('El número es muy alto');
+        }
+
         setTimeout(() => {
             
             resolve( numero + 1 );
@@ -13,14 +19,14 @@ function sumarUno( numero ) {
     return promesa;
 }
 
-sumarUno( 5 ).then( nuevoNumero => {
-    console.log( nuevoNumero );
-    return sumarUno( nuevoNumero );
-})
-.then( nuevoNumero => {
-    console.log( nuevoNumero );
-    return sumarUno( nuevoNumero );
-})
-.then( nuevoNumero => {
-    console.log( nuevoNumero );
-});
+sumarUno( 5 )
+    .then( sumarUno )
+    .then( sumarUno )  
+    .then( sumarUno )  
+    .then( sumarUno )  
+    .then( nuevoNumero => {
+        console.log( nuevoNumero );
+    })
+    .catch(err => console.log('Error en promesa', err));
+    
+    
