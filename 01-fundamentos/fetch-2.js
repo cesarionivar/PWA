1979524da394bde6a8e19807da47424d71407db3
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-14 16:16:36 -0400

    Added README

---
+++ 01-fundamentos/fetch-2.js
@@ -0,0 +1,21 @@
+// Petidición GET
+// https://reqres.in/api/users
+
+/* fetch('https://reqres.in/api/users')
+    .then( resp => resp.json())
+    .then(respObj => {
+        console.log(respObj);
+        console.log(respObj.page);
+        console.log(respObj.per_page);
+    });
+ */
+
+
+
+fetch('https://wikipedia.org')
+    .then( resp => resp.text() )
+    .then( html => {
+        document.open();
+        document.write( html );
+        document.close();
+    });
---
+++ README.md
@@ -0,0 +1,5 @@
+# PWA
+
+PWA - hace referencia a Apllicaciones Web Progresivas
+
+Prácticando el manejo de services workers para la creación de PWA
