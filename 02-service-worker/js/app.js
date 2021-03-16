e60aadcd317d3f01487b75e5b34881f994397a7c
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-15 21:02:35 -0400

    Registrando el service worker

---
+++ 02-service-worker/js/app.js
@@ -0,0 +1,10 @@
+/* if( 'serviceWorker' in navigator) {
+    console.log( 'Podemos usarlo' );
+} */
+
+// Confirmar si podemos usar el Service Worker
+if( navigator.serviceWorker ) {
+
+    navigator.serviceWorker.register('./sw.js');
+    
+}
---
+++ 02-service-worker/sw.js
@@ -0,0 +1,15 @@
+
+self.addEventListener('fetch', event => {
+    
+    if( event.request.url.includes('style.css')) {
+
+        event.respondWith(null);
+
+    } else {
+        event.respondWith(fetch(event.request));
+
+    }
+});
+    
+        
+        
