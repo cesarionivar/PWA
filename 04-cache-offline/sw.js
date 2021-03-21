20ebaba821b68fe8a488e1c39910f8e7b2ad5c10
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-21 17:42:09 -0400

    Cache network Fallback

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,8 +1,10 @@
 
+const CACHE_NAME = 'cache-1';
+
 
 self.addEventListener('install', e => {
 
-    const cacheProm = caches.open('cache-1')
+    const cacheProm = caches.open( CACHE_NAME )
         .then(cache => {
 
             return cache.addAll([
@@ -23,10 +25,31 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
 
-
+    /* == ESTRATEGIAS DEL CACHE == */
 
     // 1 - Cache Only -> Es utilizada cuando queremos que la aplicación no vuelve a la web, solo tome los recursos del cache
-    e.respondWith( caches.match(e.request) );
+    // e.respondWith( caches.match(e.request) );
 
+    // 2 - Cache with Network Fallback -> Si no lo encuentra en el cache intenta en la web
+    const respuesta = caches.match( e.request )
+        .then( res => {
+            if( res ) return res;
 
-});
+            // No existe el archivo
+            // tengo que ir a la web
+            console.log('No existe', e.request.url);
+
+            return fetch( e.request )
+                .then( newResp => {
+                    caches.open( CACHE_NAME )
+                        .then( cache => {
+                            cache.put( e.request, newResp );
+                        });
+
+                    return newResp.clone();
+                });
+        });
+
+    e.respondWith( respuesta );
+
+}); 
