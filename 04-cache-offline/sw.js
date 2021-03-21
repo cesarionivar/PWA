5dce45fa8d472846cc776891e919f90eeb856cb6
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-21 16:06:40 -0400

    Response text/html

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,13 +1,32 @@
 
 self.addEventListener('fetch',  e => {
 
-    const offlineResp = new Response(`
+    // const offlineResp = new Response(`
         
-        Bienvenido a mi Página Web
+    //     Bienvenido a mi Página Web
 
-        Disculpa, pero para usarla, necesitas internet
+    //     Disculpa, pero para usarla, necesitas internet
 
-    `);
+    // `);
+
+    const offlineResp = new Response(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Mi PWA</title>
+        </head>
+        <body class="container p-3">
+            <h1>Offline Mode</h1>
+        </body>
+        </html>
+    `, {
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    });
 
     const resp = fetch(e.request)
         .catch(() => offlineResp);
