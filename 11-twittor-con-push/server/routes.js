c0f939028810f7c079700e58b17f4cc63539e3df
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-05-13 18:55:08 -0400

    Generar la llave publica y privada

--- 11-twittor-con-push/server/routes.js
+++ 11-twittor-con-push/server/routes.js
@@ -1,6 +1,8 @@
 // Routes.js - Módulo de rutas
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const push = require('./push');
+
 
 const mensajes = [
   {
@@ -51,7 +53,10 @@ router.post('/suscribe', (req, res) => {
 // Obtener llave publica 
 router.get('/key', (req, res) => {
 
-  res.json('key publico');
+
+  const key = push.getKey();
+
+  res.json(key);
 
 });
 
